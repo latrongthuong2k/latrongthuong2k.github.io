3ab9ed62aeb6cb1bd78748046b25ceae90568677
LA TRONG THUONG <74745602+latrongthuong2k@users.noreply.github.com>
2022-12-22 13:04:24 +0900

    a

---
+++ js/main.js
@@ -0,0 +1,73 @@
+// Menu
+const menu = document.querySelector(".navbar__links");
+const menuButton = document.querySelector(".navbar__icons");
+const overlay = document.querySelector("#overlay");
+
+// menuButton
+menuButton.addEventListener("click", () => {
+  menu.classList.toggle("navbar__open");
+  menuButton.classList.toggle("open");
+  overlay.classList.toggle("show");
+});
+// overlayButton
+overlay.addEventListener("click", () => {
+  menu.classList.toggle("navbar__open");
+  menuButton.classList.toggle("open");
+  overlay.classList.toggle("show");
+});
+// window.addEventListener('scroll', function() {
+//   var header = document.querySelector('header');
+//   var scrollPosition = window.scrollY;
+//   if (scrollPosition > 0) {
+//     header.classList.add('scrolled');
+//   } else {
+//     header.classList.remove('scrolled');
+//   }
+// });
+
+window.addEventListener('load', fadeInImages);
+window.addEventListener('load', fadeInText);
+
+function fadeInImages() {
+  const images = document.querySelectorAll('.fade-in-image');
+  const options = {
+    threshold: 0
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('fade-in');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, options);
+  images.forEach(image => {
+    observer.observe(image);
+  });
+}
+//================================================================ 
+// Text
+function fadeInText() {
+  const text = document.querySelectorAll('.fade-in-text');
+  const options = {
+    threshold: 0
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('fade-in');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, options);
+
+  text.forEach(t => {
+    observer.observe(t);
+  });
+}
+
+//================================================================
+// video
+document.querySelector("video").muted = false;

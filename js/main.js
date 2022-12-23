f880e984e102c2e179d8931140edb14140fab70b
LA TRONG THUONG <74745602+latrongthuong2k@users.noreply.github.com>
2022-12-24 03:15:21 +0900

    fix

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ overlay.addEventListener("click", () => {
 window.addEventListener('scroll', function() {
   var header = document.querySelector('header');
   var scrollPosition = window.scrollY;
-  if (scrollPosition > 300) {
+  if (scrollPosition > 0) {
     header.classList.add('scrolled');
   } else {
     header.classList.remove('scrolled');
